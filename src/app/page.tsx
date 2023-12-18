8611e44bf15b978c2721061faad0b838a57fb6f8
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-12-18 21:52:00 +1000

    duration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,110 @@
-import { Account } from "@/components/Account";
-import { Balance } from "@/components/Balance";
+"use client";
+
 import { ConnectKitButton } from "@/components/ConnectKitButton";
-import { Connected } from "@/components/Connected";
-import { JuiceboxProjectOwner } from "@/components/JuiceboxProjectOwner";
-import { NetworkSwitcher } from "@/components/NetworkSwitcher";
+import { formatDuration } from "date-fns";
+import {
+  DecayRate,
+  RedemptionRate,
+  ReservedRate,
+  RulesetWeight,
+} from "@/lib/juicebox/datatypes";
+import {
+  useJbControllerCurrentRulesetOf,
+  useJbProjectsOwnerOf,
+} from "@/lib/juicebox/hooks/contract";
+import { FixedInt } from "fpnum";
+
+function formatSeconds(totalSeconds: number) {
+  const secondsPerDay = 86400;
+  const secondsPerHour = 3600;
+  const secondsPerMinute = 60;
+
+  const days = Math.floor(totalSeconds / secondsPerDay);
+  const hours = Math.floor((totalSeconds % secondsPerDay) / secondsPerHour);
+  const minutes = Math.floor(
+    (totalSeconds % secondsPerHour) / secondsPerMinute
+  );
+  const seconds = totalSeconds % secondsPerMinute;
+
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+}
+
+function useProject(projectId: bigint) {
+  const { data: owner } = useJbProjectsOwnerOf({
+    args: [projectId],
+  });
+
+  const { data: ruleset } = useJbControllerCurrentRulesetOf({
+    args: [projectId],
+    select([ruleset, metadata]) {
+      return {
+        data: {
+          ...ruleset,
+          weight: new RulesetWeight(ruleset.weight),
+          decayRate: new DecayRate(ruleset.decayRate),
+        },
+        metadata: {
+          ...metadata,
+          redemptionRate: new RedemptionRate(metadata.redemptionRate),
+          reservedRate: new ReservedRate(metadata.reservedRate),
+        },
+      };
+    },
+  });
+
+  return {
+    owner,
+    ruleset,
+  };
+}
 
 export function Page() {
+  const { owner, ruleset } = useProject(2n);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        wagmi + ConnectKit + Next.js + juicebox
-      </h1>
+    <main className="container mx-auto px-4">
+      <nav className="flex justify-between py-4">
+        <span>juice-v4</span> <ConnectKitButton />
+      </nav>
 
-      <ConnectKitButton />
+      <h1 className="text-3xl font-bold mb-8">juice-v4</h1>
 
-      <JuiceboxProjectOwner />
-
-      <Connected>
-        <hr />
-        <h2>Network</h2>
-        <NetworkSwitcher />
-        <br />
-        <hr />
-        <h2>Account</h2>
-        <Account />
-        <br />
-        <hr />
-        <h2>Balance</h2>
-        <Balance />
-      </Connected>
-    </>
+      <h2 className="font-bold mb-2">Ruleset</h2>
+      <dl className="divide-y divide-gray-100">
+        <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm font-medium leading-6 text-gray-900">
+            Duration
+          </dt>
+          <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {formatSeconds(Number(ruleset?.data.duration ?? 0))}
+          </dd>
+        </div>
+        <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm font-medium leading-6 text-gray-900">
+            Decay rate
+          </dt>
+          <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {ruleset?.data.decayRate.format()}%
+          </dd>
+        </div>
+        <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm font-medium leading-6 text-gray-900">
+            Redemption rate
+          </dt>
+          <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {ruleset?.metadata.redemptionRate.formatPercentage()}%
+          </dd>
+        </div>
+        <div className="px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt className="text-sm font-medium leading-6 text-gray-900">
+            Reserved rate
+          </dt>
+          <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {ruleset?.metadata.reservedRate.formatPercentage()}%
+          </dd>
+        </div>
+      </dl>
+    </main>
   );
 }
 
